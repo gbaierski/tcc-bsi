a86e552928c608bd380659e684e812e1721d04a4
gbaierski <gustavobaierski@gmail.com>
2023-03-24 23:00:54 -0300

    feat: Criando itens no menu de navegação e melhorando scrollbar

--- src/views/PrimeiroLayout.vue
+++ src/views/PrimeiroLayout.vue
@@ -24,7 +24,16 @@
     </section>
   </header>
   <svg id="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none"><path fill="azure" fill-opacity="1" d="M0,96L30,106.7C60,117,120,139,180,133.3C240,128,300,96,360,80C420,64,480,64,540,101.3C600,139,660,213,720,240C780,267,840,245,900,245.3C960,245,1020,267,1080,250.7C1140,235,1200,181,1260,176C1320,171,1380,213,1410,234.7L1440,256L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path></svg>
-  <section id="navegation"></section>
+  <section id="navegation">
+    <p class="navegation-item">Hambúrgueres</p>
+    <p class="navegation-item">Hotdogs</p>
+    <p class="navegation-item">Porções</p>
+    <p class="navegation-item">Refrigerantes</p>
+    <p class="navegation-item">Sucos</p>
+    <p class="navegation-item">Cervejas</p>
+    <p class="navegation-item">Outras bebidas</p>
+    <p class="navegation-item">Sobremesas</p>
+  </section>
 </template>
 
 <style lang="scss">
@@ -34,6 +43,7 @@ $positive: rgb(66, 218, 104);
 $negative: rgb(255, 38, 0);
 $main: azure;
 $contrast: #e8effa;
+$complementary: grey;
 
 
 body {
@@ -50,6 +60,18 @@ header {
   background-repeat: no-repeat;
   background-size: cover;
   background-position-y: 60%;
+}
+
+::-webkit-scrollbar {
+  width: 12px;
+}
+
+::-webkit-scrollbar-track {
+  background: $main;
+}
+
+::-webkit-scrollbar-thumb {
+  background: $complementary;
 }
 
 #header-content {
@@ -92,13 +114,30 @@ header {
 
 #navegation {
   width: 100%;
-  height: 60px;
+  height: 70px;
   background-color: $main;
-  transform: translateY(-255px);  
+  transform: translateY(-255px);
+  box-shadow: rgba(0, 0, 0, 0.01) 0px 20px 25px -5px, rgba(0, 0, 0, 0.01) 0px 10px 10px -5px;
+  display: flex;
+  justify-content: center;
+  gap: 50px;
 }
 
 .place-info-box {
   display: flex;
   gap: 5px;
+}
+
+.navegation-item {
+  font-size: 20px;
+  font-family: "Source Sans Pro", sans-serif;
+  color: $complementary;
+  transition: all 0.2s;
+}
+
+.navegation-item:hover {
+  color: black;
+  transform: translateY(-3px);
+  cursor: pointer;
 }
 </style>
