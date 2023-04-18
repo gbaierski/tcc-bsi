d0dcb7b3aacb77cf82659c370c324f9279227e8e
gbaierski <gustavobaierski@gmail.com>
2023-04-18 00:36:59 -0300

    feat: Adicionando itens na cardápio

--- src/views/PrimeiroLayout.vue
+++ src/views/PrimeiroLayout.vue
@@ -34,6 +34,112 @@
     <p class="navegation-item">Outras bebidas</p>
     <p class="navegation-item">Sobremesas</p>
   </section>
+  <section id="menu-itens">
+
+    <h2 class="menu-category">Hambúrgueres</h2>
+    <div class="menu-item">
+      <div class="item-information">
+        <h3 class="item-name">Salada</h3>
+        <div class="item-description">Pão cervejinha, 1x hambúrguer de 180g de costela, 1,5x queijo mussarela, maionese, molho especial, alface, tomate, cebola roxa e picles. Acompanha ketchup e molho especial separadamente.</div>
+        <div class="item-price">R$20,00</div>
+      </div>
+      <img class="item-image" src="../assets/img/itens/salada.jpg">
+    </div>
+    <div class="menu-item">
+      <div class="item-information">
+        <h3 class="item-name">Duplo Cheddar</h3>
+        <div class="item-description">Pão brioche, 2x hambúrguer de 180g de costela, 3 camadas de queijo cheddar, maionese, bacon picadinho. Acompanha ketchup separadamente.</div>
+        <div class="item-price">R$32,00</div>
+      </div>
+      <img class="item-image" src="../assets/img/itens/duploCheddar.jpg">
+    </div>
+    <div class="menu-item">
+      <div class="item-information">
+        <h3 class="item-name">Simples</h3>
+        <div class="item-description">Pão brioche, 1x hambúrguer de 150g de costela, 1x queijo mussarela, maionese, molho especial, alface e tomate.</div>
+        <div class="item-price">R$15,00</div>
+      </div>
+      <img class="item-image" src="../assets/img/itens/simples.jpg">
+    </div>
+    <div class="menu-item">
+      <div class="item-information">
+        <h3 class="item-name">Bersk's Burger</h3>
+        <div class="item-description">Pão brioche, 2x hambúrgueres de 180g de costela, 3 camadas de queijo mussarela, barbecue, maionese, molho especial, alface americana, tomate, cebola picadinha e picles. Acompanha ketchup, barbecue, molho especial e uma porção de batata com cheddar e bacon separadamente.</div>
+        <div class="item-price">R$70,00</div>
+      </div>
+      <img class="item-image" src="../assets/img/itens/bersk.jpg">
+    </div>
+    <div class="menu-item">
+      <div class="item-information">
+        <h3 class="item-name">Intense Burger</h3>
+        <div class="item-description">Pão brioche, 1x hambúrguer de 150g de costela, queijo gorgonzola, cebola roxa crispy, 2x tiras de bacon, molho especial. Acompanha molho especial separadamente.</div>
+        <div class="item-price">R$29,00</div>
+      </div>
+      <img class="item-image" src="../assets/img/itens/intense.jpg">
+    </div>
+    <div class="menu-item">
+      <div class="item-information">
+        <h3 class="item-name">Flipicante</h3>
+        <div class="item-description">Pão australiano, 1x hambúrguer de 200g de linguiça blumenau, queijo gorgonzola e queijo coalho, geléia de pimenta, molho especial, alface e cebola roxa chapeada. Acompanha ketchup, barbecue e molho especial separadamente.</div>
+        <div class="item-price">R$41,00</div>
+      </div>
+      <img class="item-image" src="../assets/img/itens/flipicante.jpg">
+    </div>
+    <div class="menu-item">
+      <div class="item-information">
+        <h3 class="item-name">Queso Caliente</h3>
+        <div class="item-description">Pão brioche, 1x hambúrguer de 150g de costela, queijo empanado, geléia de pimenta, rúcula e tomate. Acompanha molho especial separadamente.</div>
+        <div class="item-price">R$49,00</div>
+      </div>
+      <img class="item-image" src="../assets/img/itens/queso.jpg">
+    </div>
+
+    <h2 class="menu-category">Hotdogs</h2>
+    <div class="menu-item">
+      <div class="item-information">
+        <h3 class="item-name">Dog Simples</h3>
+        <div class="item-description">Pão 25cm, salsicha, maionese, ketchup e mostarda. Acompanha ketchup e maionese separadamente.</div>
+        <div class="item-price">R$20,00</div>
+      </div>
+      <img class="item-image" src="../assets/img/itens/dogsimples.jpg">
+    </div>
+    <div class="menu-item">
+      <div class="item-information">
+        <h3 class="item-name">Dog Cheddar Bacon</h3>
+        <div class="item-description">Pão 25cm, salsicha, maionese, ketchup, cheddar derretido e bacon picadinho. Acompanha ketchup e maionese separadamente.</div>
+        <div class="item-price">R$26,00</div>
+      </div>
+      <img class="item-image" src="../assets/img/itens/dogcheddar.jpg">
+    </div>
+    <div class="menu-item">
+      <div class="item-information">
+        <h3 class="item-name">Chickão</h3>
+        <div class="item-description">Pão 35cm, 4x salsichas, maionese, ketchup, milho, bacon picadinho, frango desfiado, calabresa e molho verde. Acompanha ketchup e maionese separadamente.</div>
+        <div class="item-price">R$40,00</div>
+      </div>
+      <img class="item-image" src="../assets/img/itens/chickao.jpg">
+    </div>
+
+    <h2 class="menu-category">Porções</h2>
+    <div class="menu-item">
+      <div class="item-information">
+        <h3 class="item-name">O Comilão</h3>
+        <div class="item-description">1 kg de batata frita, muito cheddar, 500g de coração de galinha, 500g de frango chapeado, 350g bacon em tiras, 500g de linguiça calabresa, 500g de picanha fatiada. Acompanha salada. Acompanha ketchup e maionese separadamente.</div>
+        <div class="item-price">R$220,00</div>
+      </div>
+      <img class="item-image" src="../assets/img/itens/ocomilao.jpg">
+    </div>
+
+    <h2 class="menu-category">Sobremesas</h2>
+    <div class="menu-item">
+      <div class="item-information">
+        <h3 class="item-name">Coração Mole</h3>
+        <div class="item-description">Bolo de chocolate utilizando Leite Moça Nestlé, Cacau em pó e granulado. Serve 2 pessoas.</div>
+        <div class="item-price">R$30,00</div>
+      </div>
+      <img class="item-image" src="../assets/img/itens/coracaoMole.jpg">
+    </div>
+  </section>
 </template>
 
 <style lang="scss">
@@ -44,7 +150,6 @@ $negative: rgb(255, 38, 0);
 $main: azure;
 $contrast: #e8effa;
 $complementary: grey;
-
 
 body {
   background-color: $contrast;
@@ -109,18 +214,28 @@ header {
 #wave {
   width: 100%;
   height: 300px;
-  transform: translateY(-250px);
+  position: absolute;
+  transform: translateY(-300px);
 }
 
 #navegation {
   width: 100%;
   height: 70px;
   background-color: $main;
-  transform: translateY(-255px);
   box-shadow: rgba(0, 0, 0, 0.01) 0px 20px 25px -5px, rgba(0, 0, 0, 0.01) 0px 10px 10px -5px;
   display: flex;
   justify-content: center;
   gap: 50px;
+}
+
+#menu-itens {
+  width: calc(100% - 50px);
+  padding: 25px;
+  gap: 25px;
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  flex-direction: column;
 }
 
 .place-info-box {
@@ -139,5 +254,61 @@ header {
   color: black;
   transform: translateY(-3px);
   cursor: pointer;
+}
+
+.menu-category {
+  width: 900px;
+  margin-bottom: 0px;
+  color: $complementary;
+  text-align: left;
+  font-family: "Source Sans Pro", sans-serif;
+}
+
+.menu-item {
+  background-color: $main;
+  width: 800px;
+  height: 100px;
+  padding: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: rgba(0, 0, 0, 0.01) 0px 20px 25px -5px, rgba(0, 0, 0, 0.01) 0px 10px 10px -5px;
+  font-family: "Source Sans Pro", sans-serif;
+  transition: all 0.2s;
+}
+
+.menu-item:hover {
+  cursor: pointer;
+  transform: translateY(-3px);
+}
+
+.item-information {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 10px;
+  width: 70%;
+}
+
+.item-name {
+  margin: 0px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  font-weight: lighter;
+}
+
+.item-description {
+  width: 90%;
+  color: $complementary;
+}
+
+.item-price {
+  font-size: 20px;
+}
+
+.item-image {
+  width: 150px;
+  height: 150px;
+  background-color: $complementary;
 }
 </style>
