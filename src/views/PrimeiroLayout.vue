63dd8a96c5f59edd03de087b1aadf60ff2890631
gbaierski <gustavobaierski@gmail.com>
2023-07-19 18:14:49 -0300

    fix: Corrigindo problema no carregamento da imagem no modal e preço dos itens

--- src/views/PrimeiroLayout.vue
+++ src/views/PrimeiroLayout.vue
@@ -5,55 +5,58 @@ export default {
       imagesPath: "src/assets/img/items/",
       isCartOpen: false,
       isItemOpen: false,
+      isImageLoaded: false,
+      hasAdditional: false,
+      itemImageUrl: "src/assets/img/items/default.webp",
       menu: {
         hamburgers: [
           {
             id: 1,
             name: "Salada",
             description: "Pão cervejinha, 1x hambúrguer de 180g de costela, 1,5x queijo mussarela, maionese, molho especial, alface, tomate, cebola roxa e picles. Acompanha ketchup e molho especial separadamente.",
-            price: "R$20,00",
+            price: "20,00",
             image: "salada.webp"
           },
           {
             id: 2,
             name: "Duplo Cheddar",
             description: "Pão brioche, 2x hambúrguer de 180g de costela, 3 camadas de queijo cheddar, maionese, bacon picadinho. Acompanha ketchup separadamente.",
-            price: "R$32,00",
+            price: "32,00",
             image: "duploCheddar.webp"
           },
           {
             id: 3,
             name: "Simples",
             description: "Pão brioche, 1x hambúrguer de 150g de costela, 1x queijo mussarela, maionese, molho especial, alface e tomate.",
-            price: "R$15,00",
+            price: "15,00",
             image: "simples.webp"
           },
           {
             id: 4,
             name: "Bersk's Burger",
             description: "Pão brioche, 2x hambúrgueres de 180g de costela, 3 camadas de queijo mussarela, barbecue, maionese, molho especial, alface americana, tomate, cebola picadinha e picles. Acompanha ketchup, barbecue, molho especial e uma porção de batata com cheddar e bacon separadamente.",
-            price: "R$70,00",
+            price: "70,00",
             image: "bersk.webp"
           },
           {
             id: 5,
             name: "Intense Burger",
             description: "Pão brioche, 1x hambúrguer de 150g de costela, queijo gorgonzola, cebola roxa crispy, 2x tiras de bacon, molho especial. Acompanha molho especial separadamente.",
-            price: "R$29,00",
+            price: "29,00",
             image: "intense.webp"
           },
           {
             id: 6,
             name: "Flipicante",
             description: "Pão australiano, 1x hambúrguer de 200g de linguiça blumenau, queijo gorgonzola e queijo coalho, geléia de pimenta, molho especial, alface e cebola roxa chapeada. Acompanha ketchup, barbecue e molho especial separadamente.",
-            price: "R$41,00",
+            price: "41,00",
             image: "flipicante.webp"
           },
           {
             id: 7,
             name: "Queso Caliente",
             description: "Pão brioche, 1x hambúrguer de 150g de costela, queijo empanado, geléia de pimenta, rúcula e tomate. Acompanha molho especial separadamente.",
-            price: "R$49,00",
+            price: "49,00",
             image: "queso.webp"
           }
         ],
@@ -62,21 +65,21 @@ export default {
             id: 8,
             name: "Dog Simples",
             description: "Pão 25cm, salsicha, maionese, ketchup e mostarda. Acompanha ketchup e maionese separadamente.",
-            price: "R$20,00",
+            price: "20,00",
             image: "dogsimples.webp"
           },
           {
             id: 9,
             name: "Dog Cheddar Bacon",
             description: "Pão 25cm, salsicha, maionese, ketchup, cheddar derretido e bacon picadinho. Acompanha ketchup e maionese separadamente.",
-            price: "R$26,00",
+            price: "26,00",
             image: "dogCheddar.webp"
           },
           {
             id: 10,
             name: "Chickão",
             description: "Pão 35cm, 4x salsichas, maionese, ketchup, milho, bacon picadinho, frango desfiado, calabresa e molho verde. Acompanha ketchup e maionese separadamente.",
-            price: "R$40,00",
+            price: "40,00",
             image: "chickao.webp"
           }
         ],
@@ -85,21 +88,21 @@ export default {
             id: 11,
             name: "Fritas Simples",
             description: "400g de batata frita, muito cheddar, 100g de bacon.",
-            price: "R$25,00",
+            price: "25,00",
             image: "fritasSimples.webp"
           },
           {
             id: 12,
             name: "O Comilão",
             description: "1 kg de batata frita, muito cheddar, 500g de coração de galinha, 500g de frango chapeado, 350g bacon em tiras, 500g de linguiça calabresa, 500g de picanha fatiada. Acompanha salada. Acompanha ketchup e maionese separadamente.",
-            price: "R$220,00",
+            price: "220,00",
             image: "oComilao.webp"
           },
           {
             id: 13,
             name: "Chicken Nuggets",
             description: "500g de nuggets de frango. Acompanha Ketchup e a maionese especial.",
-            price: "R$30,00",
+            price: "30,00",
             image: "nuggets.webp"
           }
         ],
@@ -108,28 +111,28 @@ export default {
             id: 14,
             name: "Laranja",
             description: "600ml de suco de laranja natural.",
-            price: "R$10,00",
+            price: "10,00",
             image: "sucoLaranja.webp"
           },
           {
             id: 15,
             name: "Uva",
             description: "600ml de suco de uva natural.",
-            price: "R$10,00",
+            price: "10,00",
             image: "sucoUva.webp"
           },
           {
             id: 16,
             name: "Abacaxi com Hortelã",
             description: "600ml de suco de abacaxi com hortelã.",
-            price: "R$10,00",
+            price: "10,00",
             image: "sucoAbacaxi.webp"
           },
           {
             id: 17,
             name: "Limonada",
             description: "400ml de limonada.",
-            price: "R$10,00",
+            price: "10,00",
             image: "sucoLimonada.webp"
           }
         ],
@@ -138,21 +141,21 @@ export default {
             id: 18,
             name: "Refrigerante",
             description: "Refrigerante 2L.",
-            price: "R$14,00",
+            price: "14,00",
             image: "refrigerante.webp"
           },
           {
             id: 19,
             name: "Água com/sem gás",
             description: "700ml de água com ou sem gás.",
-            price: "R$4,00",
+            price: "4,00",
             image: "agua.webp"
           },
           {
             id: 20,
             name: "Caipirinha de vinho",
             description: "400ml de caipirinha de vinho.",
-            price: "R$14,00",
+            price: "14,00",
             image: "caipirinhaVinho.webp"
           }
         ],
@@ -161,21 +164,21 @@ export default {
             id: 21,
             name: "Coração Mole",
             description: "Bolo de chocolate utilizando leite condensado, cacau em pó e granulado. Serve 2 pessoas.",
-            price: "R$30,00",
+            price: "30,00",
             image: "coracaoMole.webp"
           },
           {
             id: 22,
             name: "Casquinha",
             description: "Casquinha de sorvete de baunilha com cobertura de chocolate.",
-            price: "R$7,00",
+            price: "7,00",
             image: "casquinha.webp"
           },
           {
             id: 22,
             name: "Bersk's Mix",
             description: "Pote de 1L de sorvete de baunilha com cobertura de chocolate e pedaços de chocolate. Serve de 2 à 4 pessoas.",
-            price: "R$18,00",
+            price: "18,00",
             image: "bersksMix.webp"
           }
         ]
@@ -196,12 +199,42 @@ export default {
         this.isCartOpen = false;
     },
 
-    openItem() {
+    async openItem(type, id) { 
+      let item = this.menu[type].find(item => item.id === id);
+      document.getElementById("item-title-modal").innerHTML = item.name;
+      document.getElementById("item-description-modal").innerHTML = item.description;
+      document.getElementById("item-price-modal").innerHTML = 'R$' + item.price;
+
+      if(type == 'hamburgers' || type == 'hotdogs') {
+        this.hasAdditional = true;
+        console.log('sim');
+      }
+      else
+        this.hasAdditional = false;
+
+      // Pré-carrega a imagem e aguarda o carregamento completo
+      const preloadedImage = await this.preloadImage(this.imagesPath + item.image);
+
       this.isItemOpen = true;
+
+      setTimeout(() => {
+        this.itemImageUrl = preloadedImage.src;
+        this.isImageLoaded = true;
+      }, 200);
+    },
+
+    preloadImage(url) {
+      return new Promise((resolve, reject) => {
+        const image = new Image();
+        image.onload = () => resolve(image);
+        image.onerror = reject;
+        image.src = url;
+      });
     },
 
     closeItem() {
       this.isItemOpen = false;
+      this.itemImageUrl = this.imagesPath + "default.webp";
     },
   }
 }
@@ -235,13 +268,13 @@ export default {
     <div id="options-modal" :class="{'options-modal-open' : isItemOpen}" >
       <button type="button" id="modal-back" class="button" @click="closeItem()">VOLTAR</button>
       <div id="options-modal-item">
-        <img class="item-image-modal" src="../assets/img/items/bersk.webp">
-        <div class="item-information-modal">
-          <h2 class="item-title-modal">Bersk's Burger</h2>
-          <div class="item-description-modal">Pão brioche, 2x hambúrgueres de 180g de costela, 3 camadas de queijo mussarela, barbecue, maionese, molho especial, alface americana, tomate, cebola picadinha e picles. Acompanha ketchup, barbecue, molho especial e uma porção de batata com cheddar e bacon separadamente.</div>
+        <img id="item-image-modal" v-if="isImageLoaded" :src="itemImageUrl">
+        <div id="item-information-modal">
+          <h2 id="item-title-modal"></h2>
+          <div id="item-description-modal"></div>
         </div>
       </div>
-      <div id="item-additional-modal">
+      <div id="item-additional-modal" :class="{'item-additional-modal-open': hasAdditional}">
         <div class="item-additional-box">
           <h3 class="item-additional-title">Remover</h3>
           <ul class="item-additional-list">
@@ -261,7 +294,7 @@ export default {
           </ul>
         </div>
       </div>
-      <button type="button" id="modal-add" class="button">ADICIONAR R$70,00</button>
+      <button type="button" id="modal-add" class="button">ADICIONAR <div id="item-price-modal"></div></button>
     </div>
   </header>
   <svg id="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none"><path fill="azure" fill-opacity="1" d="M0,96L30,106.7C60,117,120,139,180,133.3C240,128,300,96,360,80C420,64,480,64,540,101.3C600,139,660,213,720,240C780,267,840,245,900,245.3C960,245,1020,267,1080,250.7C1140,235,1200,181,1260,176C1320,171,1380,213,1410,234.7L1440,256L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path></svg>
@@ -320,61 +353,61 @@ export default {
   </section>
   <section id="menu-items">
     <h2 class="menu-category" ref="hamburgers">Hambúrgueres</h2>
-    <div class="menu-item" v-for="hamburger in menu.hamburgers" :key="hamburger.id" @click="openItem()">
+    <div class="menu-item" v-for="hamburger in menu.hamburgers" :key="hamburger.id" @click="openItem('hamburgers', hamburger.id)">
       <div class="item-information">
         <h3 class="item-name">{{ hamburger.name }}</h3>
         <div class="item-description">{{ hamburger.description }}</div>
-        <div class="item-price">{{ hamburger.price }}</div>
+        <div class="item-price">{{ 'R$' + hamburger.price }}</div>
       </div>
       <img class="item-image" :src="imagesPath + hamburger.image">
     </div>
 
     <h2 class="menu-category" ref="hotdogs">Hotdogs</h2>
-    <div class="menu-item" v-for="hotdog in menu.hotdogs" :key="hotdog.id">
+    <div class="menu-item" v-for="hotdog in menu.hotdogs" :key="hotdog.id" @click="openItem('hotdogs', hotdog.id)">
       <div class="item-information">
         <h3 class="item-name">{{ hotdog.name }}</h3>
         <div class="item-description">{{ hotdog.description }}</div>
-        <div class="item-price">{{ hotdog.price }}</div>
+        <div class="item-price">{{ 'R$' + hotdog.price }}</div>
       </div>
       <img class="item-image" :src="imagesPath + hotdog.image">
     </div>
 
     <h2 class="menu-category" ref="servings">Porções</h2>
-    <div class="menu-item" v-for="serving in menu.servings" :key="serving.id">
+    <div class="menu-item" v-for="serving in menu.servings" :key="serving.id" @click="openItem('servings', serving.id)">
       <div class="item-information">
         <h3 class="item-name">{{ serving.name }}</h3>
         <div class="item-description">{{ serving.description }}</div>
-        <div class="item-price">{{ serving.price }}</div>
+        <div class="item-price">{{ 'R$' + serving.price }}</div>
       </div>
       <img class="item-image" :src="imagesPath + serving.image">
     </div>
 
     <h2 class="menu-category" ref="juices">Sucos</h2>
-    <div class="menu-item" v-for="juice in menu.juices" :key="juice.id">
+    <div class="menu-item" v-for="juice in menu.juices" :key="juice.id" @click="openItem('juices', juice.id)">
       <div class="item-information">
         <h3 class="item-name">{{ juice.name }}</h3>
         <div class="item-description">{{ juice.description }}</div>
-        <div class="item-price">{{ juice.price }}</div>
+        <div class="item-price">{{ 'R$' + juice.price }}</div>
       </div>
       <img class="item-image" :src="imagesPath + juice.image">
     </div>
 
     <h2 class="menu-category" ref="otherDrinks">Outras bebidas</h2>
-    <div class="menu-item" v-for="otherDrink in menu.otherDrinks" :key="otherDrink.id">
+    <div class="menu-item" v-for="otherDrink in menu.otherDrinks" :key="otherDrink.id" @click="openItem('otherDrinks', otherDrink.id)">
       <div class="item-information">
         <h3 class="item-name">{{ otherDrink.name }}</h3>
         <div class="item-description">{{ otherDrink.description }}</div>
-        <div class="item-price">{{ otherDrink.price }}</div>
+        <div class="item-price">{{ 'R$' + otherDrink.price }}</div>
       </div>
       <img class="item-image" :src="imagesPath + otherDrink.image">
     </div>
 
     <h2 class="menu-category" ref="desserts">Sobremesas</h2>
-    <div class="menu-item" v-for="dessert in menu.desserts" :key="dessert.id">
+    <div class="menu-item" v-for="dessert in menu.desserts" :key="dessert.id" @click="openItem('desserts', dessert.id)">
       <div class="item-information">
         <h3 class="item-name">{{ dessert.name }}</h3>
         <div class="item-description">{{ dessert.description }}</div>
-        <div class="item-price">{{ dessert.price }}</div>
+        <div class="item-price">{{ 'R$' + dessert.price }}</div>
       </div>
       <img class="item-image" :src="imagesPath + dessert.image">
     </div>
@@ -485,13 +518,31 @@ header {
   gap: 20px;
 }
 
+#item-image-modal {
+  border-radius: 10px;
+  width: 250px;
+  align-self: center;
+}
+
+#item-title-modal {
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  font-weight: lighter;
+  font-family: "Source Sans Pro", sans-serif;
+}
+
+#item-description-modal {
+  color: $complementary;
+  font-size: 18px;
+  font-family: "Source Sans Pro", sans-serif;
+}
+
 #item-additional-modal {
   width: 100%;
-  display: flex;
+  display: none;
   justify-content: center;
   gap: 20px;
 }
-
 
 #modal-add {
   background-color: black;
@@ -615,23 +666,8 @@ header {
   transform: scale(1,1) !important;
 }
 
-.item-image-modal {
-  border-radius: 10px;
-  width: 250px;
-  align-self: center;
-}
-
-.item-title-modal {
-  letter-spacing: 1px;
-  text-transform: uppercase;
-  font-weight: lighter;
-  font-family: "Source Sans Pro", sans-serif;
-}
-
-.item-description-modal {
-  color: $complementary;
-  font-size: 18px;
-  font-family: "Source Sans Pro", sans-serif;
+.item-additional-modal-open {
+  display: flex !important;
 }
 
 .item-additional-box {
