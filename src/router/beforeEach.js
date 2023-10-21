6cbbc824b93551debb458dc6f39f1e48ba709bfc
gbaierski <gustavobaierski@gmail.com>
2023-10-21 01:40:52 -0300

    feat: Criando lógica para impedir que o usuário entre na tela do quiz sem preencher os valores

--- src/router/beforeEach.js
+++ src/router/beforeEach.js
@@ -1,14 +1,15 @@
+import { useDataStore } from '@/stores/dataStore';
+
 export default async function (to, from, next) {
     if(to.name === 'layout1' || to.name === 'layout2')
         localStorage.setItem('passed-'+to.name, true);
 
     if(to.name === 'quiz') {
-        let passedLayout1Validation = localStorage.getItem('passed-layout1');
-        let passedLayout2Validation = localStorage.getItem('passed-layout2');
+        const dataStore = useDataStore();
+        dataStore.loadFromLocalStorage();
         
-        if(!passedLayout1Validation || !passedLayout2Validation) {
+        if(!dataStore.layout1.stepCount || !dataStore.layout2.stepCount)
             next({name:'index'});
-        }
     }
 
     next();
