64662b6a4a81189aa4df1e0e336f99f2045c9cbc
gbaierski <gustavobaierski@gmail.com>
2023-10-26 23:11:05 -0300

    fix: Adicionando dataStore no carregamento da página

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup>
+    import { onMounted } from "vue";
     import { RouterLink, RouterView } from "vue-router";
+    import { useDataStore } from "@/stores/dataStore";
+
+    const dataStore = useDataStore();
+
+    onMounted(() => {
+        dataStore.loadFromLocalStorage();
+    });
 </script>
 
 <template>  
